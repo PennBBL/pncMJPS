49927bdd6e6d8c4fc081d1e104651deb01391a68
Adon Rosen <arose@adons-mbp.uphs.upenn.edu>
2017-06-12 08:05:57 -0400

    Testing functions

--- scripts/02_runGamm/functions/functions.R
+++ scripts/02_runGamm/functions/functions.R
@@ -13,10 +13,7 @@ runGamModel <- function(dataFrame, grepPattern, qualityIndex){
   mjCol <- which(names(dataFrame)=='marcat')
   psCol <- which(names(dataFrame)=='goassessDxpmr7')
   # Declare som values for which will be returned from the gam function
-  mjUserPS <- NULL
-  mjUserTD <- NULL
-  nonUserPS <- NULL
-  nonUserTD <- NULL
+  output <- NULL
   # Now loop through each region and run the model 
   for(i in columnIndex){
     # Get the roi name
@@ -25,6 +22,41 @@ runGamModel <- function(dataFrame, grepPattern, qualityIndex){
     tmp <- gam(dataFrame[,i] ~ s(dataFrame[,ageCol]) + dataFrame[,sexCol] + dataFrame[,mjCol]*dataFrame[,psCol] + dataFrame[,qualityCol])
     # Now grap our interaction terms
     summaryVals <- summary(tmp)
-    sigTerms <- summaryVals$p.table[7:11,4]
+    sigTerms <- summaryVals$p.table[8:11,4]
+    dim(sigTerms) <- c(1, 4)
+    # Now export the variables
+    allVals <- cbind(roiName, sigTerms)
+    output <- rbind(output, allVals)
   }
+  # Now fix the column names
+  colnames(output) <- c('ROI', 'PS-Nonuser', 'PS-User', 'TD-Nonuser', 'TD-User')
+  return(output)
+}
+
+
+# Return a 2x2 matrix given a p threshold (or perform fdr correction)
+# for the number of signifianct interactions from th output of runGamModel
+returnTable <- function(inputGamVals){
+    # First FDR correct all of the columns
+    newValues <- apply(inputGamVals[,2:5], 2, function(x) p.adjust(x, method='fdr'))
+    
+    # Now find the total number of signifianct regions
+    binaryVals <-  matrix(0, nrow=dim(newValues)[1], ncol=dim(newValues)[2])
+    
+    # Now trun the values less then our 1 .05 value to 1 in the binary matrix
+    binaryVals[which(newValues <=.05)] <- 1
+    
+    # Now prepare our output
+    output <- matrix(NA, 2, 2)
+    output[1,1] <- length(which(binaryVals[,1]==1))
+    output[1,2] <- length(which(binaryVals[,2]==1))
+    output[2,1] <- length(which(binaryVals[,3]==1))
+    output[2,2] <- length(which(binaryVals[,4]==1))
+    # Now return the output
+    return(output)
+}
+
+# Produce demographic data
+produceDemoData <- function(inputData, 'grepPattern'){
+  
 }
