e06f477697297799042849ea801132bb2ccd4064
Adon Rosen <arosen@chead.uphs.upenn.edu>
2017-08-02 12:20:43 -0400

    Added GMD and CBF to voxel wise analysis

--- scripts/05_voxelWiseAnalysis/prepData.R
+++ scripts/05_voxelWiseAnalysis/prepData.R
@@ -11,6 +11,9 @@ strucData <- strucData[-which(strucData$goassessDxpmr7==levels(strucData$goasses
 
 # Prepare the paths 
 pathVals <- paste("/data/joy/BBL/studies/pnc/n1601_dataFreeze/neuroimaging/t1struct/voxelwiseMaps_antsCt/", strucData$scanid, "_CorticalThicknessNormalizedToTemplate2mm.nii.gz", sep='')
+pathVals2 <- paste("/data/joy/BBL/studies/pnc/n1601_dataFreeze/neuroimaging/t1struct/voxelwiseMaps_gmd/", strucData$scanid, "_atropos3class_prob02SubjToTemp2mm.nii.gz", sep='')
+
+
 bblid <- strucData$bblid
 age <- strucData$ageAtScan1
 sex <- strucData$sex
@@ -23,12 +26,49 @@ marBin <- strucData$marcat
 quality <- strucData$averageManualRating
 
 
-output <- as.data.frame(cbind(bblid, pathVals, age, sex, mjDosage, psTD, race, marBin, quality))
+output <- as.data.frame(cbind(bblid, pathVals, pathVals2, age, sex, mjDosage, psTD, race, marBin, quality))
 output$age <- as.numeric(as.character(output$age))
+output$pathVals <- as.character(output$pathVals)
+output$pathVals2 <- as.character(output$pathVals2)
 output$marBin <- ordered(output$marBin)
 output$mjDosage <- ordered(output$mjDosage)
 output$psTD <- ordered(output$psTD)
 output$quality <- as.factor(output$quality)
 output$inclusion <- 1
+output$inclusionFrequent <- 0
+output$inclusionFrequent[which(output$mjDosage == 0 | output$mjDosage == 5 | output$mjDosage == 6)] <- 1 
+
 
 saveRDS(output, "ctMJPSTDInteraction.rds")
+
+
+# Now do the cbf RDS file 
+cbfData$marcat[cbfData$marcat=='MJ Frequent User'] <- 'MJ User'
+cbfData <- cbfData[-which(cbfData$marcat==levels(cbfData$marcat)[1]),]
+cbfData <- cbfData[-which(cbfData$goassessDxpmr7==levels(cbfData$goassessDxpmr7)[1]),]
+pathVals3 <- paste("/data/joy/BBL/studies/pnc/n1601_dataFreeze/neuroimaging/asl/voxelwiseMaps_cbf/", cbfData$scanid, "_asl_quant_ssT1Std.nii.gz", sep='')
+
+bblid <- cbfData$bblid
+age <- cbfData$ageAtScan1
+sex <- cbfData$sex
+qaVal <- cbfData$averageManualRating
+mjDosage <- cbfData$dosage
+psTD <- cbfData$goassessDxpmr7
+race <- cbfData$race2
+race[which(race == 3)] <- 2
+marBin <- cbfData$marcat
+quality <- cbfData$pcaslRelMeanRMSMotion
+
+output <- as.data.frame(cbind(bblid, pathVals3, age, sex, mjDosage, psTD, race, marBin, quality))
+output$age <- as.numeric(as.character(output$age))
+output$pathVals3 <- as.character(output$pathVals3)
+output$marBin <- ordered(output$marBin)
+output$mjDosage <- ordered(output$mjDosage)
+output$psTD <- ordered(output$psTD)
+output$quality <- as.numeric(output$quality)
+output$inclusion <- 1
+output$inclusionFrequent <- 0
+output$inclusionFrequent[which(output$mjDosage == 0 | output$mjDosage == 5 | output$mjDosage == 6)] <- 1 
+
+saveRDS(output, "cbfMJPSTDInteraction.rds")
+
--- scripts/05_voxelWiseAnalysis/runVoxel.sh
+++ scripts/05_voxelWiseAnalysis/runVoxel.sh
@@ -9,4 +9,61 @@ covsFormula="~age+race+quality+psTD+marBin+psTD*marBin"
 logFile="/data/joy/BBL/projects/pncMJPS/data/05_voxelWiseAnalysis/log/"
 errFile="/data/joy/BBL/projects/pncMJPS/data/05_voxelWiseAnalysis/log/"
 
-Rscript /data/joy/BBL/applications/groupAnalysis/lm_voxelwise.R -c ${subjData} -o ${outDirName} -p ${namePath} -m ${maskName} -i ${inclusionName} -u ${subjID} -f ${covsFormula} -n 3 -s 0 -r FALSE -a "fdr" -d FALSE
+#Rscript /data/joy/BBL/applications/groupAnalysis/lm_voxelwise.R -c ${subjData} -o ${outDirName} -p ${namePath} -m ${maskName} -i ${inclusionName} -u ${subjID} -f ${covsFormula} -n 3 -s 0 -r FALSE -a "fdr" -d FALSE
+
+
+# Now run cluster correction
+subjData="/data/joy/BBL/projects/pncMJPS/scripts/05_voxelWiseAnalysis/ctMJPSTDInteraction.rds"
+outDirName="/data/joy/BBL/projects/pncMJPS/data/05_voxelWiseAnalysis"
+namePath="pathVals"
+maskName="/data/joy/BBL/studies/pnc/n1601_dataFreeze/neuroimaging/t1struct/n1396_ctMask_thr9_2mm.nii.gz"
+subjID="bblid"
+inclusionName="inclusion"
+fullFormula="~age+race+quality+psTD+marBin+psTD*marBin"
+redFormula="~age+race+quality+psTD+marBin"
+splits=10
+smooth=0
+thresh=0.01
+nsim=500
+runCommand=FALSE
+
+
+# CT w/ all MJ 
+outDirName="/data/joy/BBL/projects/pncMJPS/data/05_voxelWiseAnalysis/ctAll"
+mkdir -p ${outDirName}
+#Rscript /data/joy/BBL/applications/groupAnalysis/gam_randomise.R -c $subjData -o $outDirName -p $namePath -m $maskName -i $inclusionName -u $subjID -f $fullFormula -r $redFormula -t 0.01 -n 1000 -e TRUE -s 0 -d FALSE
+
+# Run CT w/ frequent MJ
+inclusionName="inclusionFrequent"
+outDirName="/data/joy/BBL/projects/pncMJPS/data/05_voxelWiseAnalysis/ctFrequent"
+mkdir -p ${outDirName}
+#Rscript /data/joy/BBL/applications/groupAnalysis/gam_randomise.R -c $subjData -o $outDirName -p $namePath -m $maskName -i $inclusionName -u $subjID -f $fullFormula -r $redFormula -t 0.01 -n 1000 -e TRUE -s 0 -d FALSE
+
+# Now do GMD ALL
+inclusionName="inclusion"
+namePath="pathVals2"
+outDirName="/data/joy/BBL/projects/pncMJPS/data/05_voxelWiseAnalysis/gmdAll"
+mkdir -p ${outDirName}
+#Rscript /data/joy/BBL/applications/groupAnalysis/gam_randomise.R -c $subjData -o $outDirName -p $namePath -m $maskName -i $inclusionName -u $subjID -f $fullFormula -r $redFormula -t 0.01 -n 1000 -e TRUE -s 0 -d FALSE
+
+# Now do GMD w/ frequent
+outDirName="/data/joy/BBL/projects/pncMJPS/data/05_voxelWiseAnalysis/gmdFrequent"
+mkdir -p ${outDirName}
+inclusionName="inclusionFrequent"
+#Rscript /data/joy/BBL/applications/groupAnalysis/gam_randomise.R -c $subjData -o $outDirName -p $namePath -m $maskName -i $inclusionName -u $subjID -f $fullFormula -r $redFormula -t 0.01 -n 1000 -e TRUE -s 0 -d FALSE
+
+# Now do CBF
+maskName="/data/joy/BBL/studies/pnc/n1601_dataFreeze/neuroimaging/asl/n1601_PcaslCoverageMask.nii.gz"
+outDirName="/data/joy/BBL/projects/pncMJPS/data/05_voxelWiseAnalysis/cbfAll"
+mkdir -p ${outDirName}
+subjData="/data/joy/BBL/projects/pncMJPS/scripts/05_voxelWiseAnalysis/cbfMJPSTDInteraction.rds"
+inclusionName="inclusion"
+namePath="pathVals3"
+# All
+Rscript /data/joy/BBL/applications/groupAnalysis/gam_randomise.R -c $subjData -o $outDirName -p $namePath -m $maskName -i $inclusionName -u $subjID -f $fullFormula -r $redFormula -t 0.01 -n 1000 -e TRUE -s 0 -d TRUE
+
+# Frequent
+outDirName="/data/joy/BBL/projects/pncMJPS/data/05_voxelWiseAnalysis/cbfFrequent"
+mkdir -p ${outDirName}
+inclusionName="inclusionFrequent"
+Rscript /data/joy/BBL/applications/groupAnalysis/gam_randomise.R -c $subjData -o $outDirName -p $namePath -m $maskName -i $inclusionName -u $subjID -f $fullFormula -r $redFormula -t 0.01 -n 1000 -e TRUE -s 0 -d FALSE
