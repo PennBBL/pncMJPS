ce803100454546732e048cc309a7d248b02c96cc
Adon Rosen <adon.rosen@gmail.com>
2018-02-22 21:40:08 -0500

    Added a figure2 and figure3 image scripts

---
+++ classPaperScripts/figure2.R
@@ -0,0 +1,112 @@
+## Load library(s)
+install_load('ggplot2', 'grid', 'gridExtra', 'MatchIt', 'mgcv')
+
+## Load the data
+mjData <- read.csv("../data/n9462_mj_ps_cnb_fortmm.csv")
+mjData$dosage <- NA
+mjData$dosage[which(mjData$marcat=='MJ Non-User')] <- 0
+mjData$dosage[which(mjData$marcat=='MJ User' & mjData$mjpastyr=='')] <- 1
+mjData$dosage[which(mjData$mjpastyr=="Less than once a month")] <- 2
+mjData$dosage[which(mjData$mjpastyr=="About once a month")] <- 3
+mjData$dosage[which(mjData$mjpastyr=="2-3 times a month")] <- 4
+mjData$dosage[which(mjData$mjpastyr=="1-2 times a week")] <- 5
+mjData$dosage[which(mjData$mjpastyr=="3-4 times a week")] <- 6
+mjData$dosage[which(mjData$mjpastyr=="Everyday or nearly every day")] <- 7
+
+# Now load imaging data
+img.data <- read.csv('../data/imagingDataAll.csv')
+fa.data <- read.csv('../data/meanFAVals.csv')
+img.data <- merge(img.data, fa.data)
+
+# Now age regress the fa.data
+img.data$age <- img.data$ageAtScan1
+img.data$age2 <- img.data$age^2
+img.data$age3 <- img.data$ageAtScan1^3
+mod <- lm(dti_jlf_fa_MeanFA ~ age + age2 + age3, data=img.data)
+img.data$dti_jlf_fa_MeanFAar <- residuals(mod)
+img.data$mean.fa <- apply(img.data[,572:619], 1, function(x) mean(x, na.rm=T))
+
+# Now prepare all of the data
+all.data <- merge(img.data, mjData)
+all.data <- all.data[-which(all.data$dosage==1),]
+all.data.freq <- all.data[which(all.data$dosage>=6),]
+all.data$usageBin <- 0
+all.data$usageBin[all.data$dosage>1] <- 1
+all.data <- all.data[-which(is.na(all.data$dosage)),]
+all.data.freq$usageBin <- 1
+
+# Now isolate genders
+male.data <- all.data[which(all.data$sex==1),]
+female.data <- all.data[which(all.data$sex==2),]
+
+# Now create our age matched samples
+# Starting with male
+tmpDat <- male.data[c('bblid', 'scanid', 'usageBin', 'ageAtScan1', 'envSES')]
+mod <- matchit(usageBin ~ ageAtScan1 + envSES, data=tmpDat, ratio=3, na.action=na.omit)
+male.data.all <- male.data
+male.data <- male.data[as.vector(mod$match.matrix),]
+male.data <- rbind(male.data, male.data.all[which(male.data.all$usageBin==1),])
+male.data <- rbind(male.data, all.data.freq[which(all.data.freq$sex==1),])
+male.data$dti_jlf_fa_MeanFAar <- as.matrix(scale(male.data$dti_jlf_fa_MeanFAar))[1:352]
+male.data[,579:612] <- scale(male.data[,579:612])
+
+# Now do female
+tmpDat <- female.data[c('bblid', 'scanid', 'usageBin', 'ageAtScan1', 'envSES')]
+mod <- matchit(usageBin ~ ageAtScan1 + envSES, data=tmpDat, ratio=3, na.action=na.omit)
+female.data.all <- female.data
+female.data <- female.data[as.vector(mod$match.matrix),]
+female.data <- rbind(female.data, female.data.all[which(female.data.all$usageBin==1),])
+female.data <- rbind(female.data, all.data.freq[which(all.data.freq$sex==2),])
+female.data$dti_jlf_fa_MeanFAar <- as.matrix(scale(female.data$dti_jlf_fa_MeanFAar))[1:255]
+female.data[,579:612] <- scale(female.data[,579:612])
+
+## Now prepare our vaues for FA in the labels
+outVals <- NULL
+allVals <- names(male.data)[572:619]
+vals <- c(4, 35, 36, 39, 21)
+inVals <- allVals[vals]
+for(q in inVals){
+    tmpVal <- summarySE(male.data, measurevar=q, groupvars='marcat', na.rm=T)
+    colnames(tmpVal)[3] <- 'mean'
+    q <- gsub(pattern="dti_dtitk_jhulabel_fa_", replacement="", x=q)
+    tmpVal$ROI <- q
+    outVals <- rbind(outVals, tmpVal)
+}
+outVals$Gender <- 'Male'
+male.fa.vals <- outVals
+outVals <- NULL
+for(q in inVals){
+    tmpVal <- summarySE(female.data, measurevar=q, groupvars='marcat', na.rm=T)
+    colnames(tmpVal)[3] <- 'mean'
+    q <- gsub(pattern="dti_dtitk_jhulabel_fa_", replacement="", x=q)
+    tmpVal$ROI <- q
+    outVals <- rbind(outVals, tmpVal)
+}
+outVals$Gender <- 'Female'
+female.fa.vals <- outVals
+fa.vals <- rbind(male.fa.vals, female.fa.vals)
+fa.vals$marcat <- factor(fa.vals$marcat, levels=c("MJ Non-User", "MJ User", "MJ Frequent User"))
+
+## Now plot these values
+outPlot <- ggplot(fa.vals, aes(x=marcat, y=mean, fill=marcat)) +
+  geom_bar(stat='identity', position=position_dodge(), size=.1) +
+  labs(title="", x="MARCAT", y="Mean FA (z-score)") +
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=.1, position=position_dodge(.9)) +
+  theme_bw() +
+  facet_grid(Gender~ROI, scales="free") +
+  theme(legend.position="none",
+  axis.text.x = element_blank(),
+  axis.text.y = element_text(size=16, face="bold"),
+  axis.ticks.x=element_blank())
+
+# Now print the output
+png('figure2-marcatMeanValues.png', height=12, width=20, units='in', res=300)
+outPlot
+dev.off()
+q()
+
+# Now make a gam w/ the dosage value
+modFemale <- gam(mean.fa ~ s(dosage,k=2) + s(age), data=female.data)
+plotGAM(gamFit=modFemale, smooth.cov='dosage')
+modMale <- gam(mean.fa ~ s(dosage,k=2) + s(age), data=male.data)
+plotGAM(gamFit=modMale, smooth.cov='dosage')
--- classPaperScripts/figure3.R
+++ classPaperScripts/figure3.R
@@ -1,6 +1,8 @@
-# Library(s)
-install_load('MatchIt')
+## Load library(s)
+install_load('psych', 'ggplot2', 'pROC', 'ggrepel', 'caret', 'randomForest', 'MatchIt', 'glmnet', 'foreach', 'useful', 'doParallel')
+source('figure3Functions.R')
 
+## Now load the data
 # Now start loading the data down here
 # This should be run from the dta directory
 mjData <- read.csv("../data/n9462_mj_ps_cnb_fortmm.csv")
@@ -49,42 +51,15 @@ female.data.all.m <- female.data
 propValueFemale <- table(female.data$usageBin)[2]/sum(table(female.data$usageBin))
 female.data.all.m$usageBinOrig <- female.data.all.m$usageBin
 
-# Before I wirte this though I have to get the deomgraphics from the freq users
-demo.vals.male <- summarySE(data=male.data, measurevar="ageAtScan1", groupvars="marcat")
-demo.vals.female <- summarySE(data=female.data, measurevar="ageAtScan1", groupvars="marcat")
-
-## Now do the frequent users
-mjData <- read.csv("../data/n9462_mj_ps_cnb_fortmm.csv")
-mjData$dosage <- NA
-mjData$dosage[which(mjData$marcat=='MJ Non-User')] <- 1
-mjData$dosage[which(mjData$marcat=='MJ User' & mjData$mjpastyr=='')] <- 1
-mjData$dosage[which(mjData$mjpastyr=="Less than once a month")] <- 2
-mjData$dosage[which(mjData$mjpastyr=="About once a month")] <- 3
-mjData$dosage[which(mjData$mjpastyr=="2-3 times a month")] <- 4
-mjData$dosage[which(mjData$mjpastyr=="1-2 times a week")] <- 5
-mjData$dosage[which(mjData$mjpastyr=="3-4 times a week")] <- 6
-mjData$dosage[which(mjData$mjpastyr=="Everyday or nearly every day")] <- 7
-
-# Now give us all the values
-all.data <- merge(img.data, mjData)
-all.data <- all.data[-which(all.data$dosage==1),]
-all.data <- all.data[-which(is.na(all.data$dosage)),]
-all.data$usageBin <- 0
-all.data$usageBin[all.data$dosage>=6] <- 1
-all.data$usageBinOrig <- all.data$usageBin
-
-# Now prepare a sex specific values
-male.data <- all.data[which(all.data$sex==1),]
-male.data <- male.data[complete.cases(male.data[,c(grep('dti_dtitk_jhulabel_fa', names(male.data)))]),]
-female.data <- all.data[which(all.data$sex==2),]
-female.data <- female.data[complete.cases(female.data[,c(grep('dti_dtitk_jhulabel_fa', names(female.data)))]),]
-sex <- rep('Male', 3)
-demo.vals.male <- rbind(demo.vals.male, summarySE(data=male.data, measurevar="ageAtScan1", groupvars="marcat")[c(1),])
-demo.vals.male <- cbind(sex, demo.vals.male)
-sex <- rep('Female', 3)
-demo.vals.female <- rbind(demo.vals.female, summarySE(data=female.data, measurevar="ageAtScan1", groupvars="marcat")[c(1),])
-demo.vals.female <- cbind(sex, demo.vals.female)
-demo.vals <- rbind(demo.vals.male, demo.vals.female)
-
-# Now write the table
-write.csv(demo.vals, "demographicVals.csv", quote=F, row.names=F)
+# Now loop through each of the label values and grab a t value between the
+# non users and the users
+valsOut <- NULL
+seqVals <- grep('_dtitk_jhulabel', names(male.data))
+for(q in seqVals){
+    tVal <-t.test(male.data[,q]~usageBin, data=male.data)
+    rocVal <- roc(usageBin~male.data[,q], data=male.data)
+    outputRow <- c(colnames(male.data)[q], as.numeric(rocVal$auc), tVal$statistic)
+    valsOut <- rbind(valsOut, outputRow)
+}
+# Now write an itk color table
+writeColorTableandKey(inputData=valsOut,inputColumn=3,outName='allValsT',minTmp=c(-3,0),maxTmp=c(0,2))
