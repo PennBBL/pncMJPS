969d961de4270eace1a5aa75a407215f7fe9e2a5
Adon Rosen <arosen@chead.uphs.upenn.edu>
2018-04-25 13:48:36 -0400

    Added hi lo guided analysis

--- scripts/10_psMJDiff/scripts/testGroupDiff.R
+++ scripts/10_psMJDiff/scripts/testGroupDiff.R
@@ -3,7 +3,7 @@
 ## This script is going to be used to grab t stats from our marcat groups 
 ## isolated to the PS groups
 
-## First load libbrary(s)
+## First load library(s)
 source('/data/joy/BBL/projects/pncMJPS/scripts/01_dataPrep/scripts/dataPrepNoFake.R')
 source('/home/arosen/adroseHelperScripts/R/afgrHelpFunc.R')
 
@@ -28,7 +28,7 @@ loopVals <- names(psOut)[grep("jlf", names(psOut))]
 loopVals <- append(loopVals, names(psOut)[grep("dtitk_jhulabel_", names(psOut))])
 loopVals <- append(loopVals, names(psOut)[grep("Nmf", names(psOut))])
 psOut <- psOut[-which(psOut$marcat=="MJ Frequent User"),]
-outputVals <- matrix(NA, 1026, 3)
+outputVals <- matrix(NA, length(loopVals), 3)
 rowCheck <- 1
 for(i in loopVals){
   formTemp <- as.formula(paste(i, "~age + age2 + age3 + sex + race2 + marcat"))
@@ -39,7 +39,7 @@ for(i in loopVals){
 }
 
 # Now perform FWE correction
-addCol <- rep(NA, 1026, 3)
+addCol <- rep(NA, length(loopVals), 3)
 grepVals <- c('vol', 'gmd', 'ct', 'cortcon', 'cbf', 'tr', 'jlf_fa', 'alff', 'reho', 'jhulabel', 'Ct', 'Ravens')
 for(gV in grepVals){
   pVals <- as.numeric(outputVals[grep(gV, outputVals[,1]),3])
@@ -53,7 +53,7 @@ psOut$age <- scale(psOut$ageAtScan1)
 psOut$age2 <- scale(psOut$ageAtScan1)^2
 psOut$age3 <- scale(psOut$ageAtScan1)^3
 psOut <- psOut[-which(psOut$marcat=="MJ User"),]
-outputVals2 <- matrix(NA, 1026, 3)
+outputVals2 <- matrix(NA, length(loopVals), 3)
 rowCheck <- 1
 for(i in loopVals){
   formTemp <- as.formula(paste(i, "~age + age2 + age3 + sex + race2 + marcat"))
@@ -64,7 +64,7 @@ for(i in loopVals){
 }
 
 # Now perform FWE correction
-addCol <- rep(NA, 1026, 3)
+addCol <- rep(NA, length(loopVals), 3)
 grepVals <- c('vol', 'gmd', 'ct', 'cortcon', 'cbf', 'tr', 'jlf_fa', 'alff', 'reho', 'jhulabel', 'Ct', 'Ravens')
 for(gV in grepVals){
   pVals <- as.numeric(outputVals2[grep(gV, outputVals[,1]),3])
@@ -72,13 +72,46 @@ for(gV in grepVals){
 }
 outputVals2 <- cbind(outputVals2,addCol)
 
+## Now do this analysis in the regions that are deemed as "important" by hi lo standards
+cog.names <- c("F1_Exec_Comp_Cog_AccuracySummedES.csv")#,
+#"F1_Slow_SpeedSummedES.csv",
+#"F1_Social_Cognition_EfficiencySummedES.csv",
+#"F2_Complex_Reasoning_EfficiencySummedES.csv",
+#"F2_Memory_SpeedSummedES.csv",
+#"F2_Social_Cog_AccuracySummedES.csv",
+#"F3_Fast_SpeedSummedES.csv",
+#"F3_Memory_AccuracySummedES.csv",
+#"F3_Memory_EfficiencySummedES.csv",
+#"F4_Executive_EfficiencySummedES.csv")
+modal.names <- c('mprage_jlf_vol_', 'mprage_jlf_gmd_', 'mprage_jlf_ct_', 'mprage_jlf_cortcon_', 'pcasl_jlf_cbf_', 'dti_jlf_tr_', 'rest_jlf_alff_', 'rest_jlf_reho_')
+# Now we want to loop through each of the cognitive metrics and read the csv, and find the ROI's that come out
+for(x in c(.7,.8,.9,1,1.1,1.2)){
+for(c in cog.names){
+  tmpVals <- read.csv(c)
+  tmpVals$sexAvg <- rowSums(tmpVals[,4:5])/2
+  importReg <- tmpVals[which(tmpVals$sexAvg>x),]
+  importReg$ROI_readable <- as.character(importReg$ROI_readable)
+  print(dim(importReg))
+  # Now go through each of our imaging modalities, and run FDR correction in the 
+  # regions that are deemed as "important"
+  outCol1 <- rep(NA, length(loopVals))
+  outCol2 <- rep(NA, length(loopVals))
+  for(m in modal.names){
+    nameVals <- gsub(x=importReg$ROI_readable, pattern='mprage_jlf_vol_', replacement=m)
+    nameVals <- append(nameVals, paste(m, "R_Pallidum", sep=''))
+    index <- which(outputVals[,1] %in% nameVals)
+    # Now apply FWE correction w/in those specific regions
+    #hist(as.numeric(outputVals[index,3]))
+    outCol1[index] <- p.adjust(as.numeric(outputVals[index,3]), method='fdr')
+    #hist( p.adjust(as.numeric(outputVals[index,3]), method='fdr'))
+    outCol2[index] <- p.adjust(as.numeric(outputVals2[index,3]), method='fdr')
+  }
+  outputVals <- cbind(outputVals, outCol1)
+  outputVals2 <- cbind(outputVals2, outCol2)
+}
+}
 ## Now we need to write the output
 toWrite1 <- outputVals[which(outputVals[,3]<.05),]
 write.csv(toWrite1, "nonVsUser.csv", quote=F, row.names=F)
 toWrite2 <- outputVals2[which(outputVals2[,3]<.05),]
 write.csv(toWrite2, "nonVsFreq.csv", quote=F, row.names=F)
-
-
-## Now explore the same analyses in a matched sample
-source('/home/arosen/adroseHelperScripts/R/afgrHelpFunc.R')
-
