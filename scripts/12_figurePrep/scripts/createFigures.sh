42b23c21634e1bb32822ca10f000657560fa80e5
Adon Rosen <arosen@chead.uphs.upenn.edu>
2019-05-15 13:41:48 -0400

    Added chead MNI template ROI values script

---
+++ scripts/12_figurePrep/scripts/createFigures.sh
@@ -0,0 +1,66 @@
+baseDir="/data/jux/BBL/projects/pncMJPS/data/"
+rData="${baseDir}colorAndKey/"
+modality=( vol tr )
+contrast=( f )
+scriptName="/home/arosen/hiLo/scripts/05_BrainRankFigure/scripts/makeZScoreJLFPNCTemplateImage.sh"
+foo() {
+  baseDir="/data/jux/BBL/projects/pncMJPS/data/"
+  rData="${baseDir}colorAndKey/"
+  m=$1
+  c=$2
+  scriptName="/home/arosen/hiLo/scripts/05_BrainRankFigure/scripts/makeZScoreJLFPNCTemplateImage.sh"
+  inputCSV="${rData}${m}${c}-KEY.csv"
+  mkdir -p ${baseDir}/imagingFigures/${m}/${c}/ 
+  cd ${baseDir}/imagingFigures/${m}/${c}/ 
+  ${scriptName} ${inputCSV} 4 0 
+  fslmaths outputImage.nii.gz -edge -bin tmp.nii.gz
+  fslmaths outputImage.nii.gz -mul tmp.nii.gz edge.nii.gz
+  fslmaths outputImage.nii.gz -sub edge.nii.gz outputImage.nii.gz
+  fslmaths tmp.nii.gz -mul 5000 tmp.nii.gz
+  fslmaths outputImage.nii.gz -add tmp.nii.gz outputImage.nii.gz 
+  rm tmp.nii.gz edge.nii.gz
+  echo "All done"
+}
+foosig() {
+  baseDir="/data/jux/BBL/projects/pncMJPS/data/"
+  rData="${baseDir}colorAndKey/"
+  m=$1
+  c=$2
+  scriptName="/home/arosen/hiLo/scripts/05_BrainRankFigure/scripts/makeZScoreJLFPNCTemplateImage.sh"
+  inputCSV="${rData}${m}${c}sig-KEY.csv"
+  mkdir -p ${baseDir}/imagingFigures/${m}/${c}sig/ 
+  cd ${baseDir}/imagingFigures/${m}/${c}sig/ 
+  ${scriptName} ${inputCSV} 4 0 
+  fslmaths outputImage.nii.gz -edge -bin tmp.nii.gz
+  fslmaths outputImage.nii.gz -mul tmp.nii.gz edge.nii.gz
+  fslmaths outputImage.nii.gz -sub edge.nii.gz outputImage.nii.gz
+  fslmaths tmp.nii.gz -mul 5000 tmp.nii.gz
+  fslmaths outputImage.nii.gz -add tmp.nii.gz outputImage.nii.gz 
+  rm tmp.nii.gz edge.nii.gz
+  echo "All done"
+}
+for s in ${contrast[*]} ; do
+    for q in ${modality[*]} ; do 
+      foo $q $s &
+      foosig $q $s &  
+    done
+done
+exit 66
+
+
+modality=( vol tr )
+contrast=( f )
+for s in ${contrast[*]} ; do 
+  for q in ${modality[*]} ; do 
+    cd ${baseDir}/imagingFigures/${q}/${s}/ 
+    itksnap -g /home/arosen/templateMNI/MNI152_T1_1mm_brain.nii.gz -s ${baseDir}/imagingFigures/${q}/${s}/outputImage.nii.gz -l ${rData}${q}${s}-ColorTable.txt & 
+  done ; 
+  #sleep 60
+done
+for s in ${contrast[*]} ; do 
+  for q in ${modality[*]} ; do 
+    cd ${baseDir}/imagingFigures/${q}/${s}sig/ 
+    itksnap -g /home/arosen/templateMNI/MNI152_T1_1mm_brain.nii.gz -s ${baseDir}/imagingFigures/${q}/${s}sig/outputImage.nii.gz -l ${rData}${q}${s}sig-ColorTable.txt & 
+  done ; 
+  #sleep 60
+done
