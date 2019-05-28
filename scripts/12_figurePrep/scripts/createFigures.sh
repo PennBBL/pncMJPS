fc31a7c6dc9db65c33fb4bb46b2680c38d565341
Adon Rosen <arosen@chead.uphs.upenn.edu>
2019-05-28 16:29:55 -0400

    Add more f stat contrasts

--- scripts/12_figurePrep/scripts/createFigures.sh
+++ scripts/12_figurePrep/scripts/createFigures.sh
@@ -1,6 +1,6 @@
 baseDir="/data/jux/BBL/projects/pncMJPS/data/"
 rData="${baseDir}colorAndKey/"
-modality=( vol tr )
+modality=( volOUvNU volFUvNU volFUvOU trOUvNU trFUvNU trFUvOU )
 contrast=( f )
 scriptName="/home/arosen/hiLo/scripts/05_BrainRankFigure/scripts/makeZScoreJLFPNCTemplateImage.sh"
 foo() {
@@ -21,34 +21,17 @@ foo() {
   rm tmp.nii.gz edge.nii.gz
   echo "All done"
 }
-foosig() {
-  baseDir="/data/jux/BBL/projects/pncMJPS/data/"
-  rData="${baseDir}colorAndKey/"
-  m=$1
-  c=$2
-  scriptName="/home/arosen/hiLo/scripts/05_BrainRankFigure/scripts/makeZScoreJLFPNCTemplateImage.sh"
-  inputCSV="${rData}${m}${c}sig-KEY.csv"
-  mkdir -p ${baseDir}/imagingFigures/${m}/${c}sig/ 
-  cd ${baseDir}/imagingFigures/${m}/${c}sig/ 
-  ${scriptName} ${inputCSV} 4 0 
-  fslmaths outputImage.nii.gz -edge -bin tmp.nii.gz
-  fslmaths outputImage.nii.gz -mul tmp.nii.gz edge.nii.gz
-  fslmaths outputImage.nii.gz -sub edge.nii.gz outputImage.nii.gz
-  fslmaths tmp.nii.gz -mul 5000 tmp.nii.gz
-  fslmaths outputImage.nii.gz -add tmp.nii.gz outputImage.nii.gz 
-  rm tmp.nii.gz edge.nii.gz
-  echo "All done"
-}
 for s in ${contrast[*]} ; do
     for q in ${modality[*]} ; do 
       foo $q $s &
-      foosig $q $s &  
+      #foosig $q $s &  
     done
 done
 exit 66
 
 
-modality=( vol tr )
+modality=( vol tr trfu )
+modality=( volOUvNU volFUvNU volFUvOU trOUvNU trFUvNU trFUvOU )
 contrast=( f )
 for s in ${contrast[*]} ; do 
   for q in ${modality[*]} ; do 
